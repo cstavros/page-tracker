201dade868e4728e681aa0a59ddd30bf69393153
cstavros <>
2023-05-31 01:59:02 -0400

    Add unit test for connection error

--- web/test/unit/test_app.py
+++ web/test/unit/test_app.py
@@ -15,3 +15,16 @@ def test_should_call_redis_incr(mock_redis, http_client):
     assert response.status_code == 200
     assert response.text == "This page has been seen 5 times."
     mock_redis.return_value.incr.assert_called_once_with("page_views")
+
+
+@unittest.mock.patch("page_tracker.app.redis")
+def test_should_handle_redis_connection_error(mock_redis, http_client):
+    # Given
+    mock_redis.return_value.incr.side_effect = ConnectionError
+
+    # When
+    response = http_client.get("/")
+
+    # Then
+    assert response.status_code == 500
+    assert response.text == "Sorry, something went wront \N{thinking face}"
