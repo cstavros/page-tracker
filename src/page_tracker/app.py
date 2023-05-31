5859d5ea8f18b16b527c72f5f88c7cabb510eb4a
cstavros <>
2023-05-31 00:44:38 -0400

    Initial commit

---
+++ src/page_tracker/app.py
@@ -0,0 +1,22 @@
+import os
+from functools import lru_cache
+
+from flask import Flask
+from redis import Redis, RedisError
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def index():
+    try:
+        page_views = redis().incr("page_views")
+    except RedisError:
+        app.logger.exception("Redis error")  # pylint: disable=E1101
+        return "Sorry, something went wrong \N{pensive face}", 500
+    return f"This page has been seen {page_views} times."
+
+
+@lru_cache(maxsize=None)
+def redis():
+    return Redis.from_url(os.getenv("REDIS_URL", "redis://localhost:6379"))
